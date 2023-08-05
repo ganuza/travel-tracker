2bd2e25c1b4e4d69af8cb06bad0165ffa26ae182
Adrian Zabolitzki <adrianzab@gmail.com>
2023-08-05 10:32:33 -0600

    Add .catch to fetchData

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,7 @@
 export const fetchData = (dataType) => {
   return fetch(`http://localhost:3001/api/v1/${dataType}`)
   .then(res => res.json())
+  .catch(error => console.log(error))
 }
 
 export const promises = [
