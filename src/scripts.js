581ba80c625cf3171ba0fadc6e5810cba4ebac78
Adrian Zabolitzki <adrianzab@gmail.com>
2023-08-05 10:36:19 -0600

    Create global variable mainData

--- src/scripts.js
+++ src/scripts.js
@@ -10,9 +10,12 @@ import './images/turing-logo.png'
 
 console.log('This is the JavaScript entry file - your code begins here.');
 
+const dayjs = require('dayjs');
+
 import {
   getUserDetails,
-  getUserTripsDetails
+  getUserTripsDetails,
+  getUserDestinationDetails,
 } from './data-model/user-data'
 
 import {
@@ -20,10 +23,14 @@ import {
   promises,
 } from './apiCalls'
 
+var mainData = {}
+
 window.addEventListener('load', () => {
   Promise.all(promises)
   .then(data => {
-    getUserDetails(data[0], 38)
-    getUserTripsDetails(data[1], 38)
+    mainData.userDetails = getUserDetails(data[0], 38)
+    mainData.userTrips = getUserTripsDetails(data[1], 38)
+    mainData.destinations = data[2].destinations
+    console.log(getUserDestinations(mainData))
   })
 })
